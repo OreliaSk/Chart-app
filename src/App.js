7962cb50044bff713f0e91a8fffdb0285f9fe50b
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-01-30 21:36:11 +0100

    App.js : modif imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Chart from './components/Chart';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Coucou</h1>
+        <Chart />
       </div>
     );
   }
